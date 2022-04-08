6733f82469801f3ead8fc9252c83331677449450
Achref <medachrefg@gmail.com>
2022-04-08 17:07:18 +0100

    Http methods Restful APIs

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const todos = [
 	{ id: 2, text: 'todo two' },
 	{ id: 3, text: 'todo three' },
 ];
+
 const server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Content-Type': 'application/json',
-		'X-Powered-By': 'Node.JS',
-	});
+	const { method, url } = req;
 	let body = [];
 	req
 		.on('data', (chunk) => {
@@ -17,15 +15,38 @@ const server = http.createServer((req, res) => {
 		})
 		.on('end', () => {
 			body = Buffer.concat(body).toString();
-			console.log(body);
+
+			let status = 404;
+			let response = {
+				success: false,
+				data: null,
+				error: null,
+			};
+
+			if (method === 'GET' && url === '/todos') {
+				status = 200;
+				response.success = true;
+				response.data = todos;
+			} else if (method === 'POST' && url === '/todos') {
+				const { id, text } = JSON.parse(body);
+
+				if (!id || !text) {
+					status = 400;
+					response.error = 'Bad Request you need to verify the id or the text';
+				} else {
+					todos.push({ id, text });
+					status = 201;
+					response.success = true;
+					response.data = todos;
+				}
+			}
+
+			res.writeHead(status, {
+				'Content-Type': 'application/json',
+				'X-Powered-By': 'Node.JS',
+			});
+			res.end(JSON.stringify(response));
 		});
-	res.end(
-		JSON.stringify({
-			success: true,
-			data: todos,
-		})
-	);
-	console.log(req.headers.authorization);
 });
 
 const PORT = 5000;
