977659addff089a99ccbc1c9746e52eb44c1a5a3
Achref <medachrefg@gmail.com>
2022-04-08 21:20:26 +0100

    Creating Routes  Responses In Express

--- devCampApi/server.js
+++ devCampApi/server.js
@@ -7,6 +7,39 @@ dotenv.config({
 });
 
 const app = express();
+
+app.get('/api/v1/bootcamps', (req, res, next) => {
+	res.status(200).json({
+		success: true,
+		msg: 'Show all bootcamps',
+	});
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res, next) => {
+	res.status(200).json({
+		success: true,
+		msg: `Show one bootcamp ${req.params.id}`,
+	});
+});
+
+app.post('/api/v1/bootcamps', (req, res, next) => {
+	res.status(200).json({
+		success: true,
+		msg: 'Create new bootcamps',
+	});
+});
+app.put('/api/v1/bootcamps/:id', (req, res, next) => {
+	res.status(200).json({
+		success: true,
+		msg: `Change a bootcamp ${req.params.id}`,
+	});
+});
+app.delete('/api/v1/bootcamps/:id', (req, res, next) => {
+	res.status(200).json({
+		success: true,
+		msg: `Delete a bootcamp ${req.params.id}`,
+	});
+});
 console.log(process.env.PORT);
 PORT = process.env.PORT;
 
