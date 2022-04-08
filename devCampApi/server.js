d41a506d70d77b3bfafabaa65adddc72a6f3f7b2
Achref <medachrefg@gmail.com>
2022-04-08 20:31:40 +0100

    initial express setup

---
+++ devCampApi/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+//Loads .env file contents into process.env.
+dotenv.config({
+	path: path.resolve(__dirname, './config/config.env'),
+});
+
+const app = express();
+console.log(process.env.PORT);
+PORT = process.env.PORT;
+
+app.listen(
+	PORT,
+	console.log(`server running in ${process.env.NODE_ENV} on ${PORT}`)
+);
