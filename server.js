07128742d9b7706b5dbb19e115caf89cde4b6afe
Achref <medachrefg@gmail.com>
2022-04-08 15:55:05 +0100

    Responding with data

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 
+const todos = [
+	{ id: 1, text: 'todo one' },
+	{ id: 2, text: 'todo two' },
+	{ id: 3, text: 'todo three' },
+];
 const server = http.createServer((req, res) => {
-	const { headers, url, method } = req;
-	console.log(headers, url, method);
-	res.end();
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('X-Powered-By', 'Node.JS');
+
+	res.end(
+		JSON.stringify({
+			success: true,
+			data: todos,
+		})
+	);
 });
 
 const PORT = 5000;
