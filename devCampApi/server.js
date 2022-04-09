d59d2a723b339787230a316c9e36af474bb3c294
Achref <medachrefg@gmail.com>
2022-04-09 02:38:47 +0100

    Intro to middleware(using morgan logger and showing a custom logger created by me

--- devCampApi/server.js
+++ devCampApi/server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+
+const logger = require('./middleware/logger');
+
 //Loads .env file contents into process.env.
 dotenv.config({
 	path: path.resolve(__dirname, './config/config.env'),
 });
 
 const app = express();
+
+//this is a custom logger create by me
+app.use(logger);
+
+//this is a logger for development env
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
