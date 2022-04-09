5a6547c15cc03f661062f5201c6981610a844581
Achref <medachrefg@gmail.com>
2022-04-10 00:00:22 +0100

    making some tiny changes on error.js and adding  validation errors

--- devCampApi/middleware/error.js
+++ devCampApi/middleware/error.js
@@ -5,6 +5,7 @@ const errorHandler = (err, req, res, next) => {
 	let error = { ...err };
 
 	error.message = err.message;
+	console.log(err.message);
 	//log to console for dev
 	console.log(`${err.stack}`.red);
 
@@ -13,6 +14,18 @@ const errorHandler = (err, req, res, next) => {
 		const messsage = `The Resource is not found verify the id ${err.value}`;
 		error = new ErrorResponse(messsage, 404);
 	}
+	//Mongoose Duplicated Key
+	if (err.code === 11000) {
+		const message = `Duplicate field value entred (${req.body.name}) `;
+		error = new ErrorResponse(message, 400);
+	}
+	//mongoose Validation errors
+	if ((err.name = 'ValidationError')) {
+		const message = Object.values(err.errors).map((value) => value.message);
+
+		error = new ErrorResponse(message, 400);
+	}
+
 	res.status(error.statusCode || 500).json({
 		success: false,
 		error: error.message || 'server error',
