1f1c83679f8949195920df82a72bd6c0fc0916d2
Achref <medachrefg@gmail.com>
2022-04-09 13:17:06 +0100

    Creating our first Model(Bootcamp)

--- devCampApi/config/db.js
+++ devCampApi/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const colors = require('colors');
 const connectDB = async () => {
 	const connect = await mongoose.connect(process.env.MONGO_URI);
-	console.log(`mongoDB connected:${connect.connection.host}`);
+	console.log(`mongoDB connected:${connect.connection.host}`.bold.blue);
 };
 
 module.exports = connectDB;
--- devCampApi/server.js
+++ devCampApi/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
-
+const colors = require('colors');
 const logger = require('./middleware/logger');
 
 //Loads .env file contents into process.env.
@@ -26,18 +26,19 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/bootcamps', bootcamps);
 
-console.log(process.env.PORT);
 PORT = process.env.PORT;
 
 const server = app.listen(
 	PORT,
-	console.log(`server running in ${process.env.NODE_ENV} on ${PORT}`)
+	console.log(
+		`server running in ${process.env.NODE_ENV} on ${PORT}`.yellow.bold.italic
+	)
 );
 
 //handle unhandled promise rejections
 
 process.on('unhandledRejection', (err, promise) => {
-	console.log(`Error:(${err.message})`);
+	console.log(`Error:(${err.message})`.red.underline);
 
 	//close the server && exsit process
 	server.close(() => process.exit(1));
