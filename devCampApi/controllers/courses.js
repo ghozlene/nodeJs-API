21b1a1e39981e291c5564e194159ec1fe2bdb4ea
Achref <medachrefg@gmail.com>
2022-04-14 00:24:04 +0100

    adding GET single Course & POST add Course to a bootcamp

--- devCampApi/controllers/courses.js
+++ devCampApi/controllers/courses.js
@@ -1,6 +1,7 @@
 const Course = require('../models/Course');
 const ErrorResponse = require('../utils/ErrorResponse');
 const asyncHandler = require('../middleware/asyncHandler');
+const Bootcamp = require('../models/Bootcamp');
 
 //@Description : GET all Courses
 //@Route: GET /api/v1/courses
@@ -22,5 +23,80 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 		success: true,
 		count: courses.length,
 		data: courses,
+	});
+});
+
+//@Description : GET single course
+//@Route: GET /api/v1/courses/:id
+//Access : Public
+exports.getSingleCourse = asyncHandler(async (req, res, next) => {
+	const course = await Course.findById(req.params.id).populate({
+		path: 'bootcamp',
+		select: 'name description',
+	});
+	if (!course) {
+		return next(
+			new ErrorResponse(
+				`course not found check the id of:${req.params.id}`,
+				404
+			)
+		);
+	}
+
+	res.status(200).json({
+		success: true,
+		data: course,
+	});
+});
+
+//@Description :  add course
+//@Route: POST /api/v1/boocamps/boocampId/courses
+//Access : Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+	req.body.bootcamp = req.params.bootcampId;
+
+	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+	if (!bootcamp) {
+		next(
+			new ErrorResponse(
+				`The bootcamp is not found verify the id ${req.params.bootcampId}`,
+				404
+			)
+		);
+	}
+	const course = await Course.create(req.body);
+	console.log(
+		`course has been added to the bootcamp (id): ${req.params.bootcampId}`.green
+			.inverse
+	);
+	res.status(200).json({
+		success: true,
+		data: course,
+	});
+});
+
+//@Description : Update Single course
+//@Route: PUT /api/v1/courses/:id
+//@Access : Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+	const courseUpdated = await Course.findByIdAndUpdate(
+		req.params.id,
+		req.body,
+		{
+			new: true,
+			runValidators: true,
+		}
+	);
+	if (!courseUpdated) {
+		next(
+			new ErrorResponse(
+				`The course is not found verify the id ${req.params.id}`,
+				404
+			)
+		);
+	}
+	res.status(200).json({
+		success: true,
+		data: courseUpdated,
 	});
 });
--- devCampApi/routes/courses.js
+++ devCampApi/routes/courses.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const { getCourse } = require('../controllers/courses');
+const {
+	getCourse,
+	getSingleCourse,
+	addCourse,
+} = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
-router.route('/').get(getCourse);
 
+router.route('/').get(getCourse).post(addCourse);
+router.route('/:id').get(getSingleCourse);
 module.exports = router;
