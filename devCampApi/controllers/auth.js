50fdfea7dbafd3e8e91597a80f540f9fd114b1c5
Achref <medachrefg@gmail.com>
2022-04-29 14:33:13 +0100

    Update user Details including the update of the Password

--- devCampApi/controllers/auth.js
+++ devCampApi/controllers/auth.js
@@ -97,6 +97,42 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 	});
 });
 
+//@Description : UPDATE user details
+//@Route: PUT /api/v1/auth/updatedetails
+//Access : Private
+
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+	const fieldsToUpdate = {
+		name: req.body.name,
+		email: req.body.email,
+	};
+	const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+		new: true,
+		runValidators: true,
+	});
+	res.status(200).json({
+		success: true,
+		data: user,
+	});
+});
+
+//@Description : UPDATE password
+//@Route: PUT /api/v1/auth/updatepassword
+//Access : private
+
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.user.id).select('+password');
+
+	//Check current password
+	if (!(await user.matchPassword(req.body.currentPassword))) {
+		return next(new ErrorResponse('Password is incorrect ', 401));
+	}
+
+	user.password = req.body.newPassword;
+	await user.save();
+	sendTokenResponse(user, 200, res);
+});
+
 //@Description : Forgot password
 //@Route: POST /auth/forgotpassword
 //Access : Public
